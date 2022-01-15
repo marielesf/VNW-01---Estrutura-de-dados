f4658b2b246a1750b6c5d1ff104ff69bf9bd1d8d
Mariele <mariele.fonttana@gmail.com>
2022-01-15 00:07:37 +0000

    Create condition.js

---
+++ condition.js
@@ -0,0 +1,20 @@
+let idade = 19;
+const humano = true;
+if(idade > 18 && humano){
+  console.log("MAIOR de idade");
+}//MAIOR de idade
+
+const aniverssario = "ABRIL";
+if(aniverssario == "Janeiro" || aniverssario == "Dezembro"){
+    console.log("Parabens, voce esta ficando velho!");
+}//SEM retorno
+
+const nome = "Raul";
+if(nome.startsWith("R")){
+    console.log("O rato roeu a roupa do rei de roma");
+}//O rato roeu a roupa do rei de roma
+
+const sobrenome = "Fontana";
+if(sobrenome.length > 6){
+    console.log("Sobrenome grande");
+}//Sobrenome grande
