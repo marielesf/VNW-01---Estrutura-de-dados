04fc72fef0c77b5252342c1842ff9529861986a5
Mariele <mariele.fonttana@gmail.com>
2022-01-26 23:41:06 +0000

    Create array.js

---
+++ array.js
@@ -0,0 +1,22 @@
+ // Crie um array que receba 5 itens e exiba no console.
+const nomes = ["Rosa Luxembugo", "Anne Frank", "Malala Yousafzai", "Maria da Penha", "Tia Ciata"]
+console.log(nomes)
+
+// Utilize um método para adicionar um nome ao inicio do array.
+nomes.unshift("Maria Firmina dos Reis ")
+
+// Utilize um método para remover o último nome do array.
+nomes.pop()
+
+// Utilize um método para adicionar dois nomes ao fim do array.
+nomes.push("Harriet Tubman", "Frida")
+
+
+// Utilize um método para remover o primeiro nome do array.
+nomes.shift()
+
+
+// Utilize um método para organizar em ordem crescente o seguinte array:
+// const numbers = [7,5,6,3,8,9,2,1,4]
+numbers.sort()
+
